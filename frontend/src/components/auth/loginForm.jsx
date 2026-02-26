e5b8a244e84dd629ceb02542c6f884882d8791f8
MahmoudHilmi <m.hilmiharb1@gmail.com>
2026-02-26 15:40:36 -0800

    Auth Finslized

--- frontend/src/components/auth/loginForm.jsx
+++ frontend/src/components/auth/loginForm.jsx
@@ -1,111 +1,102 @@
+import { Lock, Mail, User2Icon } from "lucide-react";
 import AuthToggle from "./authToggle";
 import { useState } from "react";
 
 const LoginForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+
+  const query = new URLSearchParams(window.location.search);
+  const urlState = query.get("state");
+  const [state, setState] = useState(urlState || "login");
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <div className="flex h-screen w-screen overflow-hidden">
-      <div className="w-full hidden md:inline-block">
-        <img
-          className="h-full"
-          src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/leftSideImage.png"
-          alt="leftSideImage"
-        />
-      </div>
-
-      <div className="w-full flex flex-col items-center justify-center">
-        <form className="md:w-96 w-80 flex flex-col items-center justify-center">
-          <h2 className="text-4xl text-gray-900 font-medium">Sign in</h2>
-          <p className="text-sm text-gray-500/90 mt-3">
-            Welcome back! Please sign in to continue
-          </p>
-
-          <button
-            type="button"
-            className="w-full mt-8 bg-gray-500/10 flex items-center justify-center h-12 rounded-full"
-          >
-            <img
-              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/googleLogo.svg"
-              alt="googleLogo"
-            />
-          </button>
-
-          <div className="flex items-center gap-4 w-full my-5">
-            <div className="w-full h-px bg-gray-300/90"></div>
-            <p className="w-full text-nowrap text-sm text-gray-500/90">
-              or sign in with email
-            </p>
-            <div className="w-full h-px bg-gray-300/90"></div>
-          </div>
-
-          <div className="flex items-center w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
-            <svg
-              width="16"
-              height="11"
-              viewBox="0 0 16 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M0 .55.571 0H15.43l.57.55v9.9l-.571.55H.57L0 10.45zm1.143 1.138V9.9h13.714V1.69l-6.503 4.8h-.697zM13.749 1.1H2.25L8 5.356z"
-                fill="#6B7280"
-              />
-            </svg>
+    <div className="flex items-center justify-center h-screen w-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white"
+      >
+        <h1 className="text-gray-900 text-3xl mt-10 font-medium">
+          {state === "login" ? "Login" : "Sign up"}
+        </h1>
+        <p className="text-gray-500 text-sm mt-2">Please {state} to continue</p>
+        {state !== "login" && (
+          <div className="flex items-center mt-6 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <User2Icon size={16} color="#6B7280" />
             <input
-              type="email"
-              placeholder="Email id"
-              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="border-none outline-none ring-0"
+              value={formData.name}
+              onChange={handleChange}
               required
             />
           </div>
+        )}
+        <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <Mail size={16} color="#6B7280" />
 
-          <div className="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
-            <svg
-              width="13"
-              height="17"
-              viewBox="0 0 13 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M13 8.5c0-.938-.729-1.7-1.625-1.7h-.812V4.25C10.563 1.907 8.74 0 6.5 0S2.438 1.907 2.438 4.25V6.8h-.813C.729 6.8 0 7.562 0 8.5v6.8c0 .938.729 1.7 1.625 1.7h9.75c.896 0 1.625-.762 1.625-1.7zM4.063 4.25c0-1.406 1.093-2.55 2.437-2.55s2.438 1.144 2.438 2.55V6.8H4.061z"
-                fill="#6B7280"
-              />
-            </svg>
-            <input
-              type="password"
-              placeholder="Password"
-              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
-              required
-            />
-          </div>
-
-          <div className="w-full flex items-center justify-between mt-8 text-gray-500/80">
-            <div className="flex items-center gap-2">
-              <input className="h-5" type="checkbox" id="checkbox" />
-              <label className="text-sm" htmlFor="checkbox">
-                Remember me
-              </label>
-            </div>
-            <a className="text-sm underline" href="#">
-              Forgot password?
-            </a>
-          </div>
-
-          <button
-            type="submit"
-            className="mt-8 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity"
-          >
-            Login
+          <input
+            type="email"
+            name="email"
+            placeholder="Email id"
+            className="border-none outline-none ring-0"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+<Lock size={16} color="#6B7280" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="border-none outline-none ring-0"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mt-4 text-left text-green-500">
+          <button className="text-sm" type="reset">
+            Forget password?
           </button>
-          <p className="text-gray-500/90 text-sm mt-4">
-            Don't have an account?{" "}
-            <AuthToggle isLogin={isLogin} setIsLogin={setIsLogin} />
-          </p>
-        </form>
-      </div>
+        </div>
+        <button
+          type="submit"
+          className="mt-2 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity"
+        >
+          {state === "login" ? "Login" : "Sign up"}
+        </button>
+        <p
+          onClick={() =>
+            setState((prev) => (prev === "login" ? "register" : "login"))
+          }
+          className="text-gray-500 text-sm mt-3 mb-11"
+        >
+          {state === "login"
+            ? "Don't have an account?"
+            : "Already have an account?"}{" "}
+          <a href="#" className="text-green-500 hover:underline">
+            click here
+          </a>
+        </p>
+      </form>
     </div>
   );
 };
